859a4104d19ac1f9d0bc9b618d7e09bef3091130
Ajay Sainy <sainyajay@gmail.com>
2017-02-20 16:59:23 +0530

    change interval to 60 min

--- webscrap.py
+++ webscrap.py
@@ -147,7 +147,7 @@ def insert_data(date,buses):
     for bus in buses:
         print(Database().push(date,bus.rtId,bus.__dict__))
 
-@sched.scheduled_job('interval', minutes=59)
+@sched.scheduled_job('interval', minutes=60)
 def main():
     print("fetch 23")
     new_rbus_list = DataGenerator().get_red_bus_list("https://www.redbus.in/search/result?fromCity=130&toCity=313&doj=23-Feb-2017&src=Pune&dst=Indore");
