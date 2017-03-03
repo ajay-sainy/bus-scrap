f3880e3b36003b647f7c4f977f03cdc709eae40b
sainyajay@gmail.com <sainyajay@gmail.com>
2017-03-03 23:56:39 +0530

    updating to heroku

--- webscrap.py
+++ webscrap.py
@@ -1,25 +1,10 @@
 import pyrebase
 import json
-import datetime as dt
 import logging
 logging.basicConfig()
 
-def current_date():    
-    return dt.datetime.now().strftime("%d-%b-%Y")
-
-def current_time():
-    return dt.datetime.now().strftime("%H:%M:%S")
-
-def current_timestamp():
-    return dt.datetime.now().strftime("%d-%b-%Y %H:%M:%S")
-
 class DataGenerator:
-    not_found_error = "Not Found"    
-    today_date = current_date()    
-    red_bus_url = "https://www.redbus.in/search/result?fromCity=130&toCity=313&doj="+today_date+"&src=Pune&dst=Indore"
-    #red_bus_url = "https://www.redbus.in/search/result?fromCity=130&toCity=313&doj=18-Feb-2017&src=Pune&dst=Indore"
-    
-            
+    not_found_error = "Not Found"        
     
     def get_red_bus_list(self,url):
         print("Fetching data from source")
@@ -40,32 +25,17 @@ class DataGenerator:
        
         return red_bus_list
 
-    def fetch_data(self,url):    
-        from urllib.request import urlopen
+    def fetch_data(self,url):        
+        from urllib.request import Request, urlopen  # Python 3        
+        
         try:
-            response = urlopen(url).read().decode('utf8')                    
+            q = Request(url)        
+            q.add_header('Referer', 'http://www.redbus.in/')
+            response = urlopen(q).read().decode('utf8')
             return json.loads(response)
         except:
             print("Exception")
             return self.not_found_error
-
-class RedBus():
-    travels_name = ""
-    fare = 0    
-    timestamp = ""
-    provider = ""
-    deptime = ""
-    available_seats = 0
-    rtId = ""
-    
-    def __init__(self,tname,fare,deptime,avseats,rtId):
-        self.travels_name = tname
-        self.fare = fare        
-        self.timestamp = current_timestamp()
-        self.provider = "redbus"
-        self.deptime = deptime
-        self.available_seats = avseats
-        self.rtId = rtId
         
 class Database:
     def fetch(self,provider):
@@ -98,27 +68,35 @@ class FireBase:
         rbus_list = [];
         try:
             for sbus in self.database.child(provider).get(self.user['idToken']).each():
-                rbus_list.append(RedBus(sbus.val().get("travels_name"),sbus.val().get("fare"),sbus.val().get("DepTime")))
-        except:
+                for b in sbus.val():
+                    rbus_list.append(
+                        RedBus(sbus.val().get(b).get("travels_name"),
+                               sbus.val().get(b).get("fare"),
+                               sbus.val().get(b).get("DepTime"),
+                               sbus.val().get(b).get("available_seats"),
+                               sbus.val().get(b).get("rtId")))
+        except Exception as e:
+            print("Exception "+str(e))
             return rbus_list
-            
         return rbus_list
         
     def push(self,provider,rtId,data):
         return self.database.child(provider+'/'+str(rtId)).push(data,self.user['idToken'])
 
 def get_changed_buses(oldbuses,newbuses):
+        
     print("Comparing old and new buses")
     print("Old Bus Length - "+str(len(oldbuses)),"New Bust Length - "+str(len(newbuses)))
     '''1. if oldbuses is empty, return new buses because all are new'''
     if len(oldbuses)==0:
-        return newbuses
+        insert_data("4-Mar-2017",newbuses)
+        return []
     
     '''2. Create today's old bus dictionary'''
     old_bus_dict = {}
-    for old in oldbuses:        
-        if old.date == current_date():            
-            old_bus_dict[old.travels_name] = old
+   
+    for old in oldbuses:
+        old_bus_dict[old.rtId] = old
 
     '''3. Traverse through new buses and check for below conditions
         a) if new bus exist in old dict and fare change, add it to changed_bus
@@ -127,9 +105,9 @@ def get_changed_buses(oldbuses,newbuses):
     changed_buses = []
     for new in newbuses:
         try:
-            if new.travels_name in old_bus_dict:
-                oldbus = old_bus_dict[new.travels_name]
-                if oldbus.fare != new.fare and oldbus.deptime == new.deptime:
+            if new.rtId in old_bus_dict:
+                oldbus = old_bus_dict[new.rtId]
+                if oldbus.fare != new.fare:
                     changed_buses.append(new)
             else:
                 changed_buses.append(new)
@@ -140,28 +118,46 @@ def get_changed_buses(oldbuses,newbuses):
     print("Total changed buses "+str(len(changed_buses)))
     return changed_buses;
 
-from apscheduler.schedulers.blocking import BlockingScheduler
-sched = BlockingScheduler()
-
+def compare_and_alert(old_buses,new_buses):
+    changed_buses = get_changed_buses(old_buses,new_buses)
+    if(len(changed_buses)>0):
+        insert_data("Updated",changed_buses)
 
 def insert_data(date,buses):
     print("pushing {} data to database".format(str(len(buses))))
     for bus in buses:
         print(Database().push(date,bus.rtId,bus.__dict__))
 
-@sched.scheduled_job('interval', minutes=60)
-def main():
-    print("fetch 23")
-    new_rbus_list = DataGenerator().get_red_bus_list("https://www.redbus.in/search/result?fromCity=130&toCity=313&doj=23-Feb-2017&src=Pune&dst=Indore");
-    insert_data("23-02-17",new_rbus_list)
-    print("fetch 24")
-    new_rbus_list1 = DataGenerator().get_red_bus_list("https://www.redbus.in/search/result?fromCity=130&toCity=313&doj=24-Feb-2017&src=Pune&dst=Indore");
-    insert_data("24-02-17",new_rbus_list1)
-    print("fetch 25")
-    new_rbus_list2 = DataGenerator().get_red_bus_list("https://www.redbus.in/search/result?fromCity=130&toCity=313&doj=25-Feb-2017&src=Pune&dst=Indore");
-    insert_data("25-02-17",new_rbus_list2)
-    #old_rbus_list = Database().fetch("redbus")    
+class RedBus():
+    travels_name = ""
+    fare = 0
+    provider = ""
+    deptime = ""
+    available_seats = 0
+    rtId = ""
+    
+    def __init__(self,tname,fare,deptime,avseats,rtId):
+        self.travels_name = tname
+        self.fare = fare        
+        self.provider = "redbus"
+        self.deptime = deptime
+        self.available_seats = avseats
+        self.rtId = rtId
+        
+from apscheduler.schedulers.blocking import BlockingScheduler
+sched = BlockingScheduler()
+       
+@sched.scheduled_job('interval', minutes=30)
+def main():    
+    new_rbus_list = DataGenerator().get_red_bus_list("https://www.redbus.in/search/result?fromCity=130&toCity=313&doj=4-Mar-2017&src=Pune&dst=Indore");    
+    
+    old_rbus_list = Database().fetch("4-Mar-2017")    
+   
+    for o in old_rbus_list:
+        print(o.available_seats)
+    compare_and_alert(old_rbus_list ,new_rbus_list );
+    
     #changed_buses = get_changed_buses(old_rbus_list,new_rbus_list)
 
 sched.start()
-#main()
+main()
